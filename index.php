4395203016864ed1e90b429f05804cde9b7ad724
Padi <padi1111.show@gmail.com>
2022-01-01 20:05:16 +0100

    Index base

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	  <meta charset="UTF-8">
+	  <title>SIWEL</title>
+	  <link rel="stylesheet" href="./style.css">
+	</head>
+	<main>
+		<body>
+		</body>
+	</main>
+</html>
